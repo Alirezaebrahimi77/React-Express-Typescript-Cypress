8d4b089c1780d194878399d00e04874a3b90a369
Alireza <alireza27163@gmail.com>
2022-08-02 20:57:40 +0300

    Prepaired server

--- backend/app.js
+++ backend/app.js
@@ -4,24 +4,30 @@ const errorMiddleware = require("./middlewares/errors")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const dotenv = require("dotenv")
+const path = require("path")
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
-if(process.env.NODE_ENV !== "PRODUCTION"){
-    dotenv.config({path: './config/config.env'})
- }
-
-
 // Import all routes
 const products = require("./routes/product")
 const auth = require("./routes/auth")
 
-
-
 app.use("/api/v1", products)
 app.use("/api/v1", auth)
+
+if(process.env.NODE_ENV !== "PRODUCTION"){
+    dotenv.config({path: './config/config.env'})
+ }
+ 
+if(process.env.NODE_ENV === "PRODUCTION"){
+    app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+    } )
+ }
 
 // Middleware to handle errors
 app.use(errorMiddleware)
